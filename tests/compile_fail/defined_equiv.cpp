4f4e6441b7adc61c2a653e5f7988b6f81a01cdf9
Lucien Guimier <lucien.guimier@laposte.net>
2019-04-14 17:45:07 +0200

    Add tests for preprocessor safety…
    … and fix a mistake found doing so.

---
+++ tests/compile_fail/defined_equiv.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "secureenumconverter.h"
+
+enum class A { A };
+enum class B { B };
+
+using SUT = SecureEnumConverter<A, B>;
+
+int main () {}
+
+#define SEC_EQUIV
+
+#define SEC_INTERNAL A
+#define SEC_EXTERNAL B
+#define SEC_MAPPING \
+    SEC_EQUIV(A::A, B::B)
+#include "secureenumconverter.inc"
