8032d59841743e93d0ee23dc21e4a7c8333c0b38
Lucien Guimier <lucien.guimier@laposte.net>
2019-04-14 15:43:37 +0200

    Add proofs of compilation fail with unhandled members

---
+++ tests/compile_fail/unhandled_external.cpp
@@ -0,0 +1,16 @@
+#include "assertions.h"
+#include "secureenumconverter.h"
+
+enum class A { A1, A2 };
+enum class B { B1, B2, B3 };
+using SUT = SecureEnumConverter<A, B>;
+
+#define SEC_INTERNAL A
+#define SEC_EXTERNAL B
+#define SEC_MAPPING \
+    SEC_EQUIV(A::A1, B::B2) \
+    SEC_EQUIV(A::A2, B::B1)
+#include "secureenumconverter.inc"
+
+int main () {
+}
