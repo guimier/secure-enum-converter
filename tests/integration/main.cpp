535ce26e98a7cfec6a1915af2fbf3fd5b220c05d
Lucien Guimier <lucien.guimier@laposte.net>
2019-04-14 16:58:21 +0200

    Fix integration test

--- tests/integration/main.cpp
+++ tests/integration/main.cpp
@@ -1,7 +1,7 @@
 #include "assertions.h"
 #include "sut.h"
 
-int main () {
+START_TEST(Integration)
     COMPARE_EQ(SUT::toExternalOpt(A::A1), B::B1);
     COMPARE_EQ(SUT::toExternalOpt(A::A2), B::B2);
-}
+END_TEST
